c5a9dca96eda45a6df70145b8d52d4ca1e63765e
Fouad <fouadayman58@gmail.com>
2021-04-19 13:58:01 +0200

    Implementing Predict and Update function in KalmanFilter class

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -26,12 +26,49 @@ void KalmanFilter::Predict() {
   /**
    * TODO: predict the state
    */
+
+    // predict the State ( using linear model to calculate the position and velocity ) 
+
+    x_ = F_ * x_;  
+
+    // update covariance matrix to track the uncertainity 
+    // adding Q_ which has information about process noise
+    P_ = F_ * P_ * F_.transpose() + Q_;
+
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
+    
+
+    // map the state vector to measurment vector 
+    VectorXd z_predicted = H_ * x_; 
+
+    // calculate erro in prediction  (actual sensor reading - predicted value from predicted state )
+    VectorXd y = z - z_predicted; 
+
+    // calculate S matrix
+    // adding R_ which has information about Measurment noise
+    MatrixXd S = H_ * P_ * H_.transpose() + R_;
+
+    MatrixXd Si = S.inverse();
+    MatrixXd PHt = P_ * Ht;
+
+    // calculate kalman gain
+    // Kalman Gain should have information about P and R to decide which will be trusted most (measurment or prediction)
+    MatrixXd K = PHt * Si;
+
+
+    // update new state
+    // KG will give a very heigh weight to measurment if Q is low , P is high and vice verse
+    x_ = x_ + (K * y);
+
+    // update the Covariance matrix 
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
